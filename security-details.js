883ee02342a9043900c9e915925573ff52cceb22
gazoubel <gazoubel@gmail.com>
2020-12-03 11:58:14 -0500

    Add files via upload

---
+++ security-details.js
@@ -0,0 +1,35 @@
+let lastSelectedWidgetId
+let widgetName = document.querySelector('#widget-name')
+let widgetInfo = document.querySelector('.widget-info')
+let editor = document.querySelector('#description')
+let placeholder = document.querySelector('.no-selected-widget')
+
+function showElement(el) {
+  el.style.display = 'block'
+}
+
+function hideElement(el) {
+  el.style.display = 'none'
+}
+
+hideElement(placeholder)
+hideElement(widgetInfo)
+
+miro.onReady(async () => {
+    const appId = await miro.getClientId();
+    const appIdKey = appId.toString();
+
+    let selectedWidgets = await miro.board.selection.get();
+    let selectedWidget = selectedWidgets[0]
+    if (selectedWidgets.length === 1) {
+        showElement(widgetInfo);
+        hideElement(placeholder);
+        saveEditorData();
+        lastSelectedWidgetId = selectedWidget.id;
+        widgetName.innerText = selectedWidget.type;
+        editor.value = 'this is the description of issue: '+ selectedWidget.metadata[appIdKey].issueKey;
+    } else {
+        showElement(placeholder);
+        hideElement(widgetInfo);
+    }
+})
