6c97099111ac628bdd5b29ce9f31e669656b7039
gazoubel <gazoubel@gmail.com>
2020-12-04 15:47:36 -0500

    Add files via upload

--- JiraAPI.js
+++ JiraAPI.js
@@ -1,16 +1,30 @@
-export default function JiraAPI() {
-    return {
-        getBasicInfo(issueKey) {
-            const jiraIssue = await fetch('http://localhost:2990/rest/api/latest/issue/'+issueKey, {method:'GET', 
-            headers: {'Authorization': 'Basic ' + btoa('admin:admin')}}).then(resp=>resp.json());
-            const fields = jiraIssue["fields"];
-            return fields;
-        },
-        getSecuredInfo(issueKey) {
-            const jiraIssue = await fetch('http://localhost:2990/rest/api/latest/issue/'+issueKey, {method:'GET', 
-            headers: {'Authorization': 'Basic ' + btoa('admin:admin')}}).then(resp=>resp.json());
-            const fields = jiraIssue["fields"];
-            return fields;
-        }
-    }
+function getBasicInfo(issueKey) {
+    const jiraIssue = await fetch('http://localhost:2990/rest/api/latest/issue/'+issueKey, {method:'GET', 
+    headers: {'Authorization': 'Basic ' + btoa('admin:admin')}}).then(resp=>resp.json());
+    const fields = jiraIssue["fields"];
+    return fields;
 }
+
+function getSecuredInfo(issueKey) {
+    const jiraIssue = await fetch('http://localhost:2990/rest/api/latest/issue/'+issueKey, {method:'GET', 
+    headers: {'Authorization': 'Basic ' + btoa('admin:admin')}}).then(resp=>resp.json());
+    const fields = jiraIssue["fields"];
+    return fields;
+}
+
+// function JiraAPI() {
+//     return {
+//         getBasicInfo(issueKey) {
+//             const jiraIssue = await fetch('http://localhost:2990/rest/api/latest/issue/'+issueKey, {method:'GET', 
+//             headers: {'Authorization': 'Basic ' + btoa('admin:admin')}}).then(resp=>resp.json());
+//             const fields = jiraIssue["fields"];
+//             return fields;
+//         },
+//         getSecuredInfo(issueKey) {
+//             const jiraIssue = await fetch('http://localhost:2990/rest/api/latest/issue/'+issueKey, {method:'GET', 
+//             headers: {'Authorization': 'Basic ' + btoa('admin:admin')}}).then(resp=>resp.json());
+//             const fields = jiraIssue["fields"];
+//             return fields;
+//         }
+//     }
+// }
