a79314405f080115b008808db946f85f3b7041c3
gazoubel <gazoubel@gmail.com>
2020-12-04 16:03:18 -0500

    Add files via upload

--- JiraAPI.js
+++ JiraAPI.js
@@ -1,17 +1,25 @@
-async function getBasicInfo(issueKey) {
-    const jiraIssue = await fetch('http://localhost:2990/rest/api/latest/issue/'+issueKey, {method:'GET', 
-    headers: {'Authorization': 'Basic ' + btoa('admin:admin')}}).then(resp=>resp.json());
-    const fields = jiraIssue["fields"];
-    return fields;
+const JiraAPI = {
+    getBasicInfo(issueKey) {
+        return fetch('http://localhost:2990/rest/api/latest/issue/'+issueKey, {method:'GET', 
+        headers: {'Authorization': 'Basic ' + btoa('admin:admin')}})
+            .then(resp=>resp.json())
+            .then(data=>data["fields"]);
+        // const jiraIssue = await fetch('http://localhost:2990/rest/api/latest/issue/'+issueKey, {method:'GET', 
+        // headers: {'Authorization': 'Basic ' + btoa('admin:admin')}}).then(resp=>resp.json());
+        // const fields = jiraIssue["fields"];
+        // return fields;
+    },
+    getSecuredInfo(issueKey) {
+        return fetch('http://localhost:2990/rest/api/latest/issue/'+issueKey, {method:'GET', 
+        headers: {'Authorization': 'Basic ' + btoa('admin:admin')}})
+            .then(resp=>resp.json())
+            .then(data=>data["fields"]);
+        // const jiraIssue = await fetch('http://localhost:2990/rest/api/latest/issue/'+issueKey, {method:'GET', 
+        // headers: {'Authorization': 'Basic ' + btoa('admin:admin')}}).then(resp=>resp.json());
+        // const fields = jiraIssue["fields"];
+        // return fields;
+    }
 }
-
-async function getSecuredInfo(issueKey) {
-    const jiraIssue = await fetch('http://localhost:2990/rest/api/latest/issue/'+issueKey, {method:'GET', 
-    headers: {'Authorization': 'Basic ' + btoa('admin:admin')}}).then(resp=>resp.json());
-    const fields = jiraIssue["fields"];
-    return fields;
-}
-
 // function JiraAPI() {
 //     return {
 //         getBasicInfo(issueKey) {
