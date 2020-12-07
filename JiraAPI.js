def6b109fd1f3f01f1e8e22b48be4118e3a9f9c9
gazoubel <gazoubel@gmail.com>
2020-12-07 10:23:30 -0500

    Add files via upload

--- JiraAPI.js
+++ JiraAPI.js
@@ -10,5 +10,19 @@ const JiraAPI = {
         headers: {'Authorization': 'Basic ' + btoa('admin:admin')}})
             .then(resp=>resp.json())
             .then(data=>data["fields"]);
-    }
+    },
+    getCardDataFromJira(issueKey) {
+		return JiraAPI.getBasicInfo(issueKey).then(fields=>{
+            return {
+                title: fields.summary, 
+                clientVisible: true,
+                card: {"customFields":[
+                    {"value":fields.status.name,"tooltip":"Status","fieldType":"string","fontColor":"#ffffff","mainColor":"#205081"},
+                    {"value":issueKey,"iconUrl":fields.issuetype.iconUrl,"tooltip":"Jira issue ("+fields.issuetype.name+")","fieldType":"string"},
+                    {"value":"","iconUrl": fields.priority.iconUrl,"tooltip":fields.priority.name,"fieldType":"string"},
+                    {"value":fields.assignee==null?'undefined':fields.assignee.name,"iconUrl":fields.assignee==null?"":fields.assignee.avatarUrl,"tooltip":"Assignee","fieldType":"string","roundedIcon":true}] }
+            };
+        })
+		
+	}
 }
