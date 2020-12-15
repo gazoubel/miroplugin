108ac23fc130d0e31e048db3f2a3d38213b67567
gazoubel <gazoubel@gmail.com>
2020-12-15 17:07:06 -0500

    Add files via upload

--- JiraAPI.js
+++ JiraAPI.js
@@ -40,10 +40,13 @@ const JiraAPI = {
                 clientVisible: true,
                 bounds:{height: "400", width:"1200"},
                 card: {"customFields":[
-                    {"value":fields.status.name,"tooltip":"Status","fieldType":"string","fontColor":"#ffffff","mainColor":"#205081"},
-                    {"value":issueKey,"iconUrl":fields.issuetype.iconUrl,"tooltip":"Jira issue ("+fields.issuetype.name+")","fieldType":"string"},
-                    {"value":"","iconUrl": fields.priority.iconUrl,"tooltip":fields.priority.name,"fieldType":"string"},
-                    {"value":fields.assignee==null?'undefined':fields.assignee.name,"iconUrl":fields.assignee==null?"":fields.assignee.avatarUrl,"tooltip":"Assignee","fieldType":"string","roundedIcon":true}] }
+                        {"value":fields.status.name,"tooltip":"Status","fieldType":"string","fontColor":"#ffffff","mainColor":"#205081"},
+                        {"value":issueKey,"iconUrl":fields.issuetype.iconUrl,"tooltip":"Jira issue ("+fields.issuetype.name+")","fieldType":"string"},
+                        {"value":"","iconUrl": fields.priority.iconUrl,"tooltip":fields.priority.name,"fieldType":"string"},
+                        {"value":fields.assignee==null?'undefined':fields.assignee.name,"iconUrl":fields.assignee==null?"":fields.assignee.avatarUrl,"tooltip":"Assignee","fieldType":"string","roundedIcon":true}
+                    ] ,
+                    "logo":{"iconUrl":"data:image/svg+xml;utf8,<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n    <image xlink:href=\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgogICAgPGcgb3BhY2l0eT0iLjIiPgogICAgICAgIDxwYXRoIGQ9Ik0yMy4wNDIgMEgxMmMwIDEuMzMuNTI1IDIuNjA1IDEuNDYgMy41NDVhNC45NyA0Ljk3IDAgMCAwIDMuNTI1IDEuNDY5aDIuMDM0djEuOTc1QzE5LjAyIDkuNzU2IDIxLjI0OSAxMiAyNCAxMlYuOTY0QS45Ni45NiAwIDAgMCAyMy4wNDIgMHoiLz4KICAgICAgICA8cGF0aCBkPSJNMTcuMDQyIDZINmMwIDEuMzMuNTI1IDIuNjA1IDEuNDYgMy41NDVhNC45NyA0Ljk3IDAgMCAwIDMuNTI1IDEuNDY5aDIuMDM0djEuOTc1QzEzLjAyIDE1Ljc1NiAxNS4yNDkgMTggMTggMThWNi45NjRBLjk2Ljk2IDAgMCAwIDE3LjA0MiA2eiIvPgogICAgICAgIDxwYXRoIGQ9Ik0xMS4wNDIgMTJIMGMwIDEuMzMuNTI1IDIuNjA1IDEuNDYgMy41NDVhNC45NyA0Ljk3IDAgMCAwIDMuNTI1IDEuNDY5aDIuMDM0djEuOTc1QzcuMDIgMjEuNzU2IDkuMjQ5IDI0IDEyIDI0VjEyLjk2NGEuOTYuOTYgMCAwIDAtLjk1OC0uOTY0eiIvPgogICAgPC9nPgo8L3N2Zz4=\" x=\"0\" y=\"0\" height=\"24\" width=\"24\"/>\n</svg>"}
+                }
             };
         })
 		
